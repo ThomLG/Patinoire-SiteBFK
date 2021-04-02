273d8eb4fd49648723bd2e60f7b41290d29983df
Le Gloux <legloux.thomas@gmail.com>
2021-04-03 00:33:48 +0200

    fin mise en page infrastructures

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\CategoryRepository;
+use App\Repository\PartnerRepository;
 use App\Repository\PlayerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,18 +12,31 @@ use Symfony\Component\Routing\Annotation\Route;
 class PlayerController extends AbstractController
 {
     /**
-     * @Route("/players", name="players")
+     * @Route("/seniors", name="seniors")
      */
-    public function index(PlayerRepository $playerRepository): Response
+    public function index(PlayerRepository $playerRepository, CategoryRepository $categoryRepository, PartnerRepository $partnerRepository): Response
     {
-        $players = $playerRepository->findAll();
-
-        $seniorsPlayers=$playerRepository->findPlayerByCategory('Seniors');
-        $femininesPlayers=$playerRepository->findPlayerByCategory('Féminines');
+        $category=$categoryRepository->findOneBy(['categoryName'=>'Seniors']);
+        $partners=$partnerRepository->findAll();
+        $players=$playerRepository->findPlayerByCategory('Seniors');
         return $this->render('player/index.html.twig', [
             'players' => $players,
-            'seniorsplayers'=>$seniorsPlayers,
-            'femininesplayers'=>$femininesPlayers
+            'partners'=>$partners,
+            'category'=>$category
+        ]);
+    }
+
+    /**
+     * @Route("/feminines", name="feminines")
+     */
+    public function showFeminines(PlayerRepository $playerRepository, CategoryRepository $categoryRepository, PartnerRepository $partnerRepository): Response{
+        $category=$categoryRepository->findOneBy(['categoryName'=>'Féminines']);
+        $players=$playerRepository->findPlayerByCategory('Féminines');
+        $partners=$partnerRepository->findAll();
+        return $this->render('player/index.html.twig', [
+            'players'=>$players,
+            'partners'=>$partners,
+            'category'=>$category
         ]);
     }
 
